cfe30f03fcc03b1199f1d59e34745364ac70588a
PriyankaDathriB <priyanka.dathri@gmail.com>
2024-07-26 10:17:23 +0530

    arguments passing

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import flask
-from flask import Flask,render_template
-
+from flask import Flask,render_template,jsonify,request
+from data import students
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    #return "My first server message"
-    return render_template('index.html')
-
-@app.route("/next")
-def next():
-    #return "My first server message"
-    return render_template('next.html',name = "anamika")
+    return jsonify({
+        "data":students,
+        "message":"success"
+    }),200
+    
+    
+#http://127.0.0.1:5000/student?name=Pravalika
+@app.route("/student")
+def student():
+    sname = request.args.get("name")
+    for item in students:
+        if sname == item["name"]:
+            return jsonify({
+                "data":item,
+                "message":"success"
+            }),200
 
 
 app.run(debug=True )
-
---
+++ data.py
@@ -0,0 +1,15 @@
+students = [
+    {   "name":"Pravalika",
+        "grade":10,
+        "age":16
+    },
+    {   "name":"Priyanka",
+        "grade":12,
+        "age":19
+    },
+    {   "name":"Pamela",
+        "grade":11,
+        "age":18
+    }
+]
+
